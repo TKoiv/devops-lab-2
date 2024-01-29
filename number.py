30da35b549cbe7315f068c5c9302d82185503be9
TKoiv <c1kota00@students.oamk.fi>
2024-01-29 14:19:52 +0000

    Add new program

---
+++ number.py
@@ -0,0 +1,9 @@
+number1 = int(input("Number 1 is: "))
+number2 = int(input("Number 2 is: "))
+number3 = int(input("Number 3 is: "))
+number4 = int(input("Number 4 is: "))
+
+def func(number1, number2, number3, number4):
+    return (number1 + number2 + number3 + number4) / 4
+
+print(func(number1, number2, number3, number4))
